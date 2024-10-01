f1577a3d84c81b1dec3fe9659022b1b6935ea1c4
Matltin <ma1383ahmadi@gmail.com>
2024-10-01 23:40:31 +0330

    add server

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "github.com/Matltin/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer create a new HTTP server and setup routing.
+func NewServer(store *db.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+
+	server.router = router
+	return server
+}
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(addres string) error {
+	return server.router.Run(addres)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
